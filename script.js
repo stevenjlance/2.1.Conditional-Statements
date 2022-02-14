3ea8d561c948d51b4a7de067c79a46f6c5ef74bd
Steven Lance <44912347+stevenjlance@users.noreply.github.com>
2022-02-14 18:53:40 +0000

    Solutions added

--- script.js
+++ script.js
@@ -16,6 +16,60 @@ function draw() {
 	// }
 
 	
+
+	// Challenge 1
+	// if (mouseX > 300) {
+  //   //if mouseX > 300 change the fill to blue
+  //   fill(0, 0, 255);
+  // }
+  // //draw a rect at the center of the canvas
+	// rectMode(CENTER)
+  // rect(width / 2, height / 2, 50, 50);
+
+	// Challenge 2
+	// var x = 10; //set the initial size to 10
+
+  // rectMode(CENTER); //set the rectMode to center
+  // background(0);
+  // fill(255, 0, 0); //set the initial fill to red
+
+  // if (mouseX > 200) {
+  //   x = 60;
+  // }
+  // //draw a rect at the center of the canvas
+  // rect(width / 2, height / 2, x, x);
+
+	// // Challenge 3
+	//  var x=40;//set the value of x to 40
+  // fill(0,255,0)//set the fill to green
+  // background(220);
+
+	// //if mouseX is on the right side of the screen,
+  // //shrink the ellipse to 20
+  // if(mouseX>width/2){
+  // 	x=20;
+  // }
+  // //if mouseY is on the botto half of the screen,
+  // //change the fill to red
+  // if(mouseY>height/2){
+  // 	fill(255,0,0);
+  // }
+  // ellipse(width/2,height/2,x,x)
+
+
+	// Challenge 4
+	// stroke(255);
+  // strokeWeight(4);
+  // noFill();
+  // if(mouseX > 100){
+  //   fill(200,255,0)
+  // }
+  // if (mouseX > 300) {
+  //   fill(255, 0, 200);
+  // }
+  // if(mouseX > 400){
+  //   fill(0, 255, 200)
+  // }
 	
 
 
